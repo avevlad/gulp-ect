02cc36baddbaa54354cfc16e0b40af908f4dd96f
mdrake <matthew.drake@22squared.com>
2014-02-06 12:04:16 -0500

    Added data support

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -3,8 +3,10 @@ var gutil = require('gulp-util');
 var ect = require('../');
 
 gulp.task('ect', function(){
-  gulp.src('./src/*.ect')
-      .pipe(ect())
+  gulp.src(['./src/*.ect','./src/inner/*.ect'])
+      .pipe(ect({data:function(file,cb){    	  
+    	  cb({foo:"bar - "+file});
+      }}))
       .pipe(gulp.dest('./out'));
 });
 
