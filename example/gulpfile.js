e8e5ec1107d896a5d9acfe64b30a5e187fe4f589
AveVlad <dev@vld.me>
2014-02-06 23:27:29 +0400

    update example

--- example/gulpfile.js
+++ example/gulpfile.js
@@ -2,14 +2,12 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var ect = require('../');
 
-gulp.task('ect', function(){
-  gulp.src(['./src/*.ect','./src/inner/*.ect'])
-      .pipe(ect({data:function(file,cb){    	  
-    	  cb({foo:"bar - "+file});
-      }}))
-      .pipe(gulp.dest('./out'));
+gulp.task('ect', function () {
+  gulp.src(['./src/*.ect', './src/inner/*.ect'])
+    .pipe(ect({data: function (file, cb) {
+      cb({foo: "bar - " + file});
+    }}))
+    .pipe(gulp.dest('./out'));
 });
 
-gulp.task('default', function(){
-  gulp.run('ect');
-});
+gulp.task('default', ['ect']);
