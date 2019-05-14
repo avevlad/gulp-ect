94119a3d586eace51747f1318a90374fb478be70
Yuki Minakawa <minakawa-yuki@dmm.com>
2019-05-14 17:56:30 +0900

    :bug: fix build error

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var gutil = require('gulp-util');
 var path = require('path');
 var ect = require('ect');
 
-var gulpEct = (options) => {
+module.exports = function (option) {
 
-  options = options || {};
-  if (!options.ext) options.ext = '.ect';
-  if (!options.data) options.data = {};
-  if (!options.outExt) options.outExt = '.html';
+  option = option || {};
+  if (!option.ext) option.ext = '.ect';
+  if (!option.data) option.data = {};
+  if (!option.outExt) option.outExt = '.html';
 
   return es.map(function (file, callback) {
     //function for compile locals async
-    var dataCallback = typeof(options.data) == 'function' ? options.data : function (file, cb) {
+    var dataCallback = typeof(option.data) == 'function' ? option.data : function (file, cb) {
       process.nextTick(function () {
-        cb(options.data);
+        cb(option.data);
       });
     };
 
@@ -22,34 +22,34 @@ var gulpEct = (options) => {
     try {
       var filePath = file.base;
       var fileCwd = file.cwd;
-      var fileName = gutil.replaceExtension(path.basename(file.path), "");
+      var fileName = gutil.replaceExtension(file.path, "");
 
       var relativeBase = path.relative(fileCwd, filePath);
       var relativePath = path.join(relativeBase, fileName);
+
       //relative path for dynamic locals creation
       dataCallback(relativePath, function (data) {
         try {
           var html = ect({
             root: filePath,
-            ext: options.ext
+            ext: option.ext
           });
 
           html.render(fileName, data, function (error, html) {
             if (error) throw new Error('[Error gulp-ect] ' + error);
+            error && gutil.log(gutil.colors.red('Error gulp-ect: ' + error.message));
             file.contents = new Buffer(html);
-            file.path = gutil.replaceExtension(file.path, options.outExt);
+            file.path = gutil.replaceExtension(file.path, option.outExt);
           });
         } catch (e) {
-          gutil.log(gutil.colors.red('Error gulp-ect: ' + e.message));
           return callback(e);
         }
         callback(null, file);
       });
     } catch (e) {
-      gutil.log(gutil.colors.red('Error Locals gulp-ect: ' + e.message));
       return callback(e);
     }
+
+
   });
 };
-
-module.exports = gulpEct;
